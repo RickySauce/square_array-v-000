ca973831055e69076a0bc81abc63fc556ca0bcfb
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 15:18:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 array = [1,2,3]
 
 def square_array(array)
-  new_array = Array.new
-  array.each do |numbers|
+  array.collect do |numbers|
     numbers **= 2
-    new_array << numbers
   end
-  return new_array
 end
